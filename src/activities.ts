fa289a6687842f217e87e1b971703a50ad207f95
gmcgoldr <garrin.mcgoldrick@gmail.com>
2022-11-07 17:33:22 -0500

    add activities

---
+++ src/activities.ts
@@ -0,0 +1,178 @@
+import { DateTime, Key, Proof, Uri, buildDocCid, sign, verify } from "./signatures.js";
+import { getIsoDate } from "./utils.js";
+import { omit } from "lodash-es";
+
+const CONTEXT_ACTIVITY_STREAMS = "https://www.w3.org/ns/activitystreams";
+const CONTEXT_CREDENTIALS = "https://www.w3.org/2018/credentials/v1";
+
+interface BaseObject {
+  attachment?: any;
+  attributedTo?: any;
+  audience?: any;
+  content?: any;
+  context?: any;
+  name?: any;
+  endTime?: any;
+  generator?: any;
+  icon?: any;
+  image?: any;
+  inReplyTo?: any;
+  location?: any;
+  preview?: any;
+  published?: any;
+  replies?: any;
+  startTime?: any;
+  summary?: any;
+  tag?: any;
+  updated?: any;
+  url?: any;
+  to?: any;
+  bto?: any;
+  cc?: any;
+  bcc?: any;
+  mediaType?: any;
+  duration?: any;
+}
+
+export interface ObjectDoc extends BaseObject {
+  "@context": string[];
+  id?: Uri;
+  type: String;
+}
+
+export async function newObjectDoc(
+  type: string,
+  members?: Omit<BaseObject, "id" | "type">
+): Promise<ObjectDoc> {
+  const objectDoc: ObjectDoc = {
+    ...members,
+    "@context": [CONTEXT_ACTIVITY_STREAMS],
+    type,
+    ...members,
+  };
+  let cid = (await buildDocCid(objectDoc)).toString();
+  objectDoc.id = `urn:cid:${cid}`;
+  return objectDoc;
+}
+
+export async function verifyObjectDoc(objectDoc: ObjectDoc): Promise<boolean> {
+  let objectDocForId = omit(objectDoc, ["id"]);
+  let cid = (await buildDocCid(objectDocForId)).toString();
+  if (`urn:cid:${cid}` !== objectDoc.id) return false;
+  return true;
+}
+
+export interface Inbox {
+  "@context": string[];
+  id: Uri;
+  type: "OrderedCollection";
+  orderedItems: Message[];
+}
+
+export function newInbox(actorId: string, messages: Message[], after?: string): Inbox {
+  const id = after != null ? `${actorId}/inbox?after=${after}` : `${actorId}/inbox`;
+  return {
+    "@context": [CONTEXT_ACTIVITY_STREAMS],
+    id,
+    type: "OrderedCollection",
+    orderedItems: messages,
+  };
+}
+
+export interface Actor extends BaseObject {
+  "@context": [string, string];
+  id: Uri;
+  type: string;
+  inbox: Uri;
+  outbox: Uri;
+  following: Uri;
+  followers: Uri;
+  proof?: Proof;
+}
+
+export async function newActor(
+  did: Uri,
+  type: string,
+  key: Key,
+  members?: Omit<BaseObject, "id" | "type">
+): Promise<Actor> {
+  if (members && !key) throw Error("actor members added without a key");
+  const id = `${did}/actor`;
+  const inbox = `${id}/inbox`;
+  const outbox = `${id}/outbox`;
+  const following = `${id}/following`;
+  const followers = `${id}/followers`;
+  const actor: Actor = {
+    "@context": [CONTEXT_ACTIVITY_STREAMS, CONTEXT_CREDENTIALS],
+    id,
+    type,
+    inbox,
+    outbox,
+    following,
+    followers,
+    ...members,
+  };
+  if (key) return await sign(actor, key);
+  return actor;
+}
+
+export function didFromActorId(actorId: string): string | undefined {
+  const [did, path] = actorId.split("/", 2);
+  if (path != "actor") return undefined;
+  if (!did.startsWith("did:")) return undefined;
+  return did;
+}
+
+export async function verifyActor(actor: Actor): Promise<boolean> {
+  const did = didFromActorId(actor.id);
+  if (!did) return false;
+  if (actor.inbox !== `${actor.id}/inbox`) return false;
+  if (actor.outbox !== `${actor.id}/outbox`) return false;
+  if (actor.following !== `${actor.id}/following`) return false;
+  if (actor.followers !== `${actor.id}/followers`) return false;
+  if (!(await verify(actor, did))) return false;
+  return true;
+}
+
+export interface Message extends BaseObject {
+  "@context": string[];
+  id?: Uri;
+  type: String;
+  actor: Uri;
+  object: Uri;
+  published: DateTime;
+  proof?: Proof;
+}
+
+export async function newMessage(
+  actorDid: Uri,
+  objectId: Uri,
+  type: string,
+  published: DateTime | null,
+  key: Key,
+  members?: Omit<BaseObject, "id" | "type" | "actor" | "object" | "published">
+): Promise<Message> {
+  let actor = `${actorDid}/actor`;
+  const message: Message = {
+    ...members,
+    "@context": [CONTEXT_ACTIVITY_STREAMS, CONTEXT_CREDENTIALS],
+    type,
+    actor,
+    object: objectId,
+    published: published != null ? published : getIsoDate(),
+    ...members,
+  };
+  let cid = (await buildDocCid(message)).toString();
+  message.id = `urn:cid:${cid}`;
+  return await sign(message, key);
+}
+
+export async function verifyMessage(message: Message): Promise<boolean> {
+  const did = didFromActorId(message.actor);
+  if (!did) return false;
+  let messageForId = omit(message, ["id", "proof"]);
+  let cid = (await buildDocCid(messageForId)).toString();
+  if (`urn:cid:${cid}` !== message.id) return false;
+  if (!(await verify(message, did))) return false;
+  return true;
+}
---
+++ test/activities.spec.ts
@@ -0,0 +1,102 @@
+import * as Activities from "../src/activities.js";
+import { didFromKey, newKey } from "../src/didkey.js";
+import { sign } from "../src/signatures.js";
+import assert from "assert";
+import { omit } from "lodash-es";
+
+describe("activities", () => {
+  const did = "did:key:z6MkqesEr2GVFXc3qWZi9PzMqtvMMyR5gB3P3R5GTsB7YTRC";
+
+  it("builds did from actor id", async () => {
+    const actorId = `${did}/actor`;
+    const didBack = Activities.didFromActorId(actorId);
+    assert.equal(did, didBack);
+  });
+
+  it("doesnt build did from invalid actor id", async () => {
+    assert.ok(!Activities.didFromActorId(`${did}/other`));
+    assert.ok(!Activities.didFromActorId(`${did}`));
+    assert.ok(!Activities.didFromActorId(`a:b/actor`));
+  });
+
+  it("builds and verifies an object with invalid ID", async () => {
+    const objectDoc = await Activities.newObjectDoc("Note", {
+      content: "abc",
+    });
+    objectDoc.content = "abcd";
+    assert.ok(!(await Activities.verifyObjectDoc(objectDoc)));
+  });
+
+  it("builds an inbox", async () => {
+    const jwk = await newKey();
+    const did = didFromKey(jwk);
+    const message = await Activities.newMessage(did, "urn:cid:a", "Create", null, jwk);
+    const inbox = Activities.newInbox("did:example:a/actor", [message], "urn:cid:a");
+    assert.equal(inbox.id, "did:example:a/actor/inbox?after=urn:cid:a");
+    assert.equal(inbox.orderedItems[0].id, message.id);
+  });
+
+  it("builds and verifies an actor", async () => {
+    const jwk = await newKey();
+    const did = didFromKey(jwk);
+    const actor = await Activities.newActor(did, "Person", jwk, { name: "abc" });
+    assert.ok(await Activities.verifyActor(actor));
+    assert.ok(actor.id.startsWith(did));
+    assert.equal(actor.name, "abc");
+  });
+
+  it("doesnt verify invalid actor", async () => {
+    const jwk = await newKey();
+    const did = didFromKey(jwk);
+    const actor = await Activities.newActor(did, "Person", jwk, { name: "abc" });
+    assert.ok(!(await Activities.verifyActor({ ...actor, name: "abcd" })));
+    assert.ok(
+      !(await Activities.verifyActor(await sign({ ...omit(actor, "proof"), inbox: "a:b" }, jwk)))
+    );
+    assert.ok(
+      !(await Activities.verifyActor(await sign({ ...omit(actor, "proof"), outbox: "a:b" }, jwk)))
+    );
+    assert.ok(
+      !(await Activities.verifyActor(
+        await sign({ ...omit(actor, "proof"), following: "a:b" }, jwk)
+      ))
+    );
+    assert.ok(
+      !(await Activities.verifyActor(
+        await sign({ ...omit(actor, "proof"), followers: "a:b" }, jwk)
+      ))
+    );
+  });
+
+  it("builds and verifies a message", async () => {
+    const jwk = await newKey();
+    const did = didFromKey(jwk);
+    const message = await Activities.newMessage(did, "urn:cid:a", "Create", null, jwk, {
+      to: "did:example:a",
+    });
+    assert.ok(await Activities.verifyMessage(message));
+    assert.ok(message.actor.startsWith(did));
+    assert.equal(message.object, "urn:cid:a");
+    assert.equal(message.to, "did:example:a");
+  });
+
+  it("doesnt verify modified message", async () => {
+    const jwk = await newKey();
+    const did = didFromKey(jwk);
+    const message = await Activities.newMessage(did, "urn:cid:a", "Create", null, jwk, {
+      to: "did:example:a",
+    });
+    message.to = "did:example:b";
+    assert.ok(!(await Activities.verifyMessage(message)));
+  });
+
+  it("doesnt verify message with invalid ID", async () => {
+    const jwk = await newKey();
+    const did = didFromKey(jwk);
+    const message = await Activities.newMessage(did, "urn:cid:a", "Create", null, jwk, {
+      to: "did:example:a",
+    });
+    let invalid = await sign({ ...omit(message, "proof"), id: "urn:cid:a" }, jwk);
+    assert.ok(!(await Activities.verifyMessage(invalid)));
+  });
+});
